1d4b2a00074cbf5e27c7f87fc23f51db020d5a3c
Meng Wang <anchor89@126.com>
2014-05-10 21:53:33 +0800

    1. Solve http://oj.leetcode.com/problems/insertion-sort-list/

--- InsertionSortList.cpp
+++ InsertionSortList.cpp
@@ -1,1 +1,23 @@
 #include "LeetCode.h"
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+  ListNode *insertionSortList(ListNode *head) {
+    ListNode h(0), *t=&h, *n;
+    while(head != NULL) {
+      for(t=&h; t->next != NULL && t->next->val < head->val; t=t->next);
+      n = head; head=head->next;
+      n->next = t->next;
+      t->next = n;
+    }
+    return h.next;
+  }
+};
