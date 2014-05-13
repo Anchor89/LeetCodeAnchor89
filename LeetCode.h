30f658be958290a1db6018c34d145033a33d455c
Meng Wang <anchor89@126.com>
2014-05-13 22:00:03 +0800

    Solve http://oj.leetcode.com/problems/unique-binary-search-trees-ii/

--- LeetCode.h
+++ LeetCode.h
@@ -13,3 +13,10 @@ struct ListNode {
   ListNode* next;
   ListNode(int x) : val(x), next(NULL) {}
 };
+
+struct TreeNode {
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
--- UniqueBinarySearchTreesII.cpp
+++ UniqueBinarySearchTreesII.cpp
@@ -1,1 +1,43 @@
 #include "LeetCode.h"
+
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+  typedef vector<int>::iterator ite;
+  void generateTrees(ite begin, ite end, vector<TreeNode*>& ret) {
+    ret.clear();
+    if (begin >= end) {
+      ret.push_back(NULL);
+      return;
+    }
+    vector<TreeNode*> lt, rht;
+    for(ite cur=begin; cur!=end; cur++) {
+      generateTrees(begin, cur, lt);
+      generateTrees(cur+1, end, rht);
+      for (int i=0; i<lt.size(); i++) {
+	for (int j=0; j<rht.size(); j++) {
+	  TreeNode* rt = new TreeNode(*cur);
+	  rt->left = lt[i];
+	  rt->right = rht[j];
+	  ret.push_back(rt);
+	}
+      }
+    }
+  }
+  vector<TreeNode *> generateTrees(int n) {
+    vector<TreeNode*> ret;
+    vector<int> nums(n);
+    int i = 1;
+    generate_n(nums.begin(), n, [&]() {return i++;});
+    generateTrees(nums.begin(), nums.end(), ret);    
+    return ret;
+  }
+};
