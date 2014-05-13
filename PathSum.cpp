6304ac86fa10bc096f2e2e97c64233a5c849c1f4
Meng Wang <anchor89@126.com>
2014-05-13 22:31:03 +0800

    Solve http://oj.leetcode.com/problems/path-sum/

--- PathSum.cpp
+++ PathSum.cpp
@@ -1,1 +1,18 @@
 #include "LeetCode.h"
+
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+  bool hasPathSum(TreeNode *root, int sum) {
+    if (!root) return false;
+    return !root->right&&!root->left? root->val==sum:hasPathSum(root->left, sum-root->val) || hasPathSum(root->right, sum-root->val);
+  }
+};
