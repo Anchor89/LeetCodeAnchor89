b4fc9f04405b2dd217e86e5d2c3d02086f0ad458
Meng Wang <anchor89@126.com>
2014-05-20 13:08:14 +0800

    Solve 2 problems

--- CopyListwithRandomPointer.cpp
+++ CopyListwithRandomPointer.cpp
@@ -1,1 +1,30 @@
 #include "LeetCode.h"
+
+/**
+ * Definition for singly-linked list with a random pointer.
+ * struct RandomListNode {
+ *     int label;
+ *     RandomListNode *next, *random;
+ *     RandomListNode(int x) : label(x), next(NULL), random(NULL) {}
+ * };
+ */
+class Solution {
+public:
+  RandomListNode *copyRandomList(RandomListNode *head) {
+    if (!head) return NULL;
+    RandomListNode *cur1, h(0), *t=&h;
+    for(cur1=head; cur1; cur1=cur1->next->next) {
+      RandomListNode* tmp = new RandomListNode(cur1->label);
+      tmp->next = cur1->next;
+      cur1->next = tmp;
+    }
+    for(cur1=head; cur1; cur1=cur1->next->next) {
+      cur1->next->random = cur1->random? cur1->random->next:NULL;
+    }
+    for(cur1=head; cur1; cur1=cur1->next) {
+      t = t->next = cur1->next;
+      cur1->next = t->next;
+    }
+    return h.next;
+  }
+};
--- ReverseLinkedListII.cpp
+++ ReverseLinkedListII.cpp
@@ -1,1 +1,29 @@
 #include "LeetCode.h"
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+  ListNode *reverseBetween(ListNode *head, int m, int n) {
+    ListNode h(0), *t1=&h,*t2,*t3;
+    h.next = head;
+    n-=m;
+    while(--m) t1=t1->next;
+    t2 = t1->next;
+    t3 = t2->next;
+    while(n--) {
+      ListNode* tmp = t3->next;
+      t3->next = t1->next;
+      t1->next = t3;
+      t3 = tmp;
+    }
+    t2->next = t3;
+    return h.next;
+  }
+};
