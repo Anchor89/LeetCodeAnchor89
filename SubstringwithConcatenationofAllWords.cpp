2538c6109f6c3e84d3e67a4619539744d7e9b020
Meng Wang <anchor89@126.com>
2014-06-20 19:27:14 +0800

    Solved

--- SubstringwithConcatenationofAllWords.cpp
+++ SubstringwithConcatenationofAllWords.cpp
@@ -1,1 +1,72 @@
 #include "LeetCode.h"
+
+class Solution {
+public:
+  vector<int> findSubstring(string S, vector<string> &L) {
+    vector<int> result;
+    if (S.empty() || L.empty()) return result;
+    int len = L[0].size();
+    int size = S.size();
+    unordered_map<string, int> limit, cur; // Use two counter
+    for ( auto& l : L) {
+      limit[l]++;
+    }
+    for (int i=0; i<len; i++) {
+      cur.clear();
+      int count = 0;
+      for (int head = i, tail=i; tail+len<=size; head+=len) {
+        string sub = S.substr(tail, len);
+        while (!sub.empty() && limit.count(sub) > 0 && cur[sub] < limit[sub]) {
+          count += ++cur[sub] == limit[sub]? 1:0;
+          tail+=len;
+          sub = tail+len <= size? S.substr(tail, len):"";
+        }
+        if (count == limit.size()) {
+          result.push_back(head);
+        }
+        if (!sub.empty() && limit.count(sub) > 0) {
+          count -= cur[S.substr(head, len)]--==limit[S.substr(head, len)]? 1:0;
+        } else {
+          cur.clear();
+          count = 0;
+          head = tail;
+          tail+=len;
+        }
+      }
+    }
+    return result;
+  }
+};
+
+class Solution {
+public:
+  vector<int> findSubstring(string S, vector<string> &L) {
+    vector<int> result;
+    if (S.empty() || L.empty()) return result;
+    int len = L[0].size();
+    int size = S.size();
+    unordered_map<string, int> limit; // Use only one counter
+    for ( auto& l : L) {
+      limit[l]++;
+    }
+    for (int i=0; i<len; i++) {
+      int count = limit.size();
+      for (int head = i, tail=i; head+len<=size; head+=len, tail = max(tail, head)) {
+        string sub = tail+len <= size? S.substr(tail, len):"";
+        while (sub.size() == len && limit.count(sub) > 0 && limit[sub] > 0) {
+          limit[sub]--;
+          count -= limit[sub] == 0? 1:0;
+          tail+=len;
+          sub = S.substr(tail, len);
+        }
+        if (count == 0) {
+          result.push_back(head);
+        }
+        if (limit.count(S.substr(head, len)) > 0) {
+          if (limit[S.substr(head, len)]++ == 0) count++;
+        }
+      }
+    }
+    return result;
+  }
+};
--- WildcardMatching.cpp
+++ WildcardMatching.cpp
@@ -1,1 +1,35 @@
 #include "LeetCode.h"
+
+class Solution {
+public:
+  bool isMatch(char s, char p) {
+    return p == '*' || p == '?' || p == s;
+  }
+  /** 
+   * dp[i][j]: length i of p and length j of s;
+   * dp[i][j] = dp[i-1][j-1] || dp[i-1][j] || dp[i][j-1] when p[i-1]=='*'
+   *                ^                 ^           ^
+   *          match s[j-1]     * match nothing    * match s[..(j-1)]
+   *          = dp[i-1][j-1] && p[i-1] == s[j-1]  when p[i-1] != '*'
+   */
+  bool isMatch(const char *s, const char *p) {
+    int lens = strlen(s), lenp = strlen(p);
+    if (lenp - count(p, p+lenp, '*') > lens) return false;
+    if (lenp == 0) return lens == 0;
+    vector<vector<bool>> dp(2, vector<bool>(lens+1, false));
+    dp[0][0] = true;
+    bool heading = true; // Deal with heading ***
+    for (int i=1; i<=lenp; i++) {
+      heading = dp[i&1][0] = (heading && p[i-1] == '*'); 
+      for (int j=1; j<=lens; j++) {
+        int pre = 1-(i&1);
+        if (p[i-1] == '*') {
+          dp[i&1][j] = dp[pre][j-1] || dp[pre][j] || dp[i&1][j-1];
+        } else {
+          dp[i&1][j] = dp[pre][j-1] && isMatch(s[j-1], p[i-1]);
+        }
+      }
+    }
+    return dp[lenp&1][lens];
+  }
+};
