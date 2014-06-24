a7258fdfd0a721178c559393dd292cbe7bc27aef
Meng Wang <anchor89.wm@gmail.com>
2014-06-25 00:10:12 +0800

    Solved

--- IntegertoRoman.cpp
+++ IntegertoRoman.cpp
@@ -1,1 +1,33 @@
 #include "LeetCode.h"
+
+class Solution {
+public:
+  string form(int num, char n1, char n5, char n10) {
+    if (num == 0) {
+      return string();
+    } else if (num < 4) {
+      return string(num, n1);
+    } else if (num == 4) {
+      return string(1, n1) + n5;
+    } else if (num == 5) {
+      return string(1, n5);
+    } else if (num < 9) {
+      return string(1, n5) + string(num-5, n1);
+    } else if (num == 9) {
+      return string(1, n1) + n10;
+    }
+  }
+  string intToRoman(int num) {
+    string result;
+    if(num > 0) {
+      result = form(num%10, 'I', 'V', 'X');
+      num/=10;
+      result = form(num%10, 'X', 'L', 'C') + result;
+      num/=10;
+      result = form(num%10, 'C', 'D', 'M') + result;
+      num/=10;
+      result = form(num%10, 'M', 'M', 'M') + result;
+    }
+    return result;
+  }
+};
--- RomantoInteger.cpp
+++ RomantoInteger.cpp
@@ -1,1 +1,41 @@
 #include "LeetCode.h"
+
+class Solution {
+public:
+  using T = string::iterator;
+  int form(T& begin, T end, char n1, char n5, char n10) {
+    int result = 0;
+    T t1m = find_if_not(begin, end, [&](char c) {return c==n1;});
+    T t5 = find_if_not(t1m, end, [&](char c) {return c==n5;});
+    T t1p = find_if_not(t5, end, [&](char c) {return c==n1;});
+    T t10 = find_if_not(t1p, end, [&](char c) {return c==n10;});
+    if (t5 != t1m) {
+      if (t1m != begin) {
+	result = 5 - distance(begin, t1m);
+      } else if (t1p != t5) {
+	result = 5 + distance(t5, t1p);
+      } else {
+	result = 5;
+      }
+    } else {
+      result = t10 == t1m? distance(begin, t1m):9;
+    }
+    begin = max(max(t1m, t5), max(t1p, t10));
+    return result;
+  }
+  int romanToInt(string s) {
+    T begin = s.begin();
+    int result = 0;
+    int radix = 1000;
+    while(begin != s.end()) {
+      result += radix*form(begin, s.end(), 'M', 'M', 'M');
+      radix/=10;
+      result += radix*form(begin, s.end(), 'C', 'D', 'M');
+      radix/=10;
+      result += radix*form(begin, s.end(), 'X', 'L', 'C');
+      radix/=10;
+      result += radix*form(begin, s.end(), 'I', 'V', 'X');
+    }
+    return result;
+  }
+};
